0a77c3e27f34368a1cd7c0d0dcc1b6aa17e65092
alvinsridar <alvinsridar@gmail.com>
2021-01-18 22:52:53 +0800

    Update app.js
    add try catch

--- NodeJS104/src/app.js
+++ NodeJS104/src/app.js
@@ -1,12 +1,14 @@
 const fs = require('fs/promises');
 
 (async () => {
-    const textA = await (await fs.readFile('./a.txt')).toString().toUpperCase();
-    const textB = await (await fs.readFile('./b.txt')).toString().toLowerCase();
-    const textC = await (await fs.readFile('./c.txt')).toString().split('').reverse().join('');
-
-    const textD = textA + textB + textC;
-
-    await fs.writeFile('./d.txt', textD.toString());
-    console.log(textD);
+    try {
+        const textA = await (await fs.readFile('./a.txt')).toString().toUpperCase();
+        const textB = await (await fs.readFile('./b.txt')).toString().toLowerCase();
+        const textC = await (await fs.readFile('./c.txt')).toString().split('').reverse().join('');
+        const textD = textA + textB + textC;
+        await fs.writeFile('./d.txt', textD.toString());
+        console.log('File written.');
+    } catch (err) {
+        console.error(err);
+    }
 })();
