1740e552dc4cf69dbc145caa9845d5cacdaf7cf5
alvinsridar <alvinsridar@gmail.com>
2021-01-18 22:54:30 +0800

    Update app.js
    removed unncessary await

--- NodeJS104/src/app.js
+++ NodeJS104/src/app.js
@@ -2,9 +2,9 @@ const fs = require('fs/promises');
 
 (async () => {
     try {
-        const textA = await (await fs.readFile('./a.txt')).toString().toUpperCase();
-        const textB = await (await fs.readFile('./b.txt')).toString().toLowerCase();
-        const textC = await (await fs.readFile('./c.txt')).toString().split('').reverse().join('');
+        const textA = (await fs.readFile('./a.txt')).toString().toUpperCase();
+        const textB = (await fs.readFile('./b.txt')).toString().toLowerCase();
+        const textC = (await fs.readFile('./c.txt')).toString().split('').reverse().join('');
         const textD = textA + textB + textC;
         await fs.writeFile('./d.txt', textD.toString());
         console.log('File written.');
