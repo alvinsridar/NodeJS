4980a40974c6f79d5c73f3978a6a216034061fa4
alvinsridar <alvinsridar@gmail.com>
2021-02-13 20:10:43 +0800

    NodeJS Assignment 12
    1. Implement JWT token on user login and register API
    -responding with JWT token upon successful login and signup
    
    2. Use authentication middleware to protect routes
    -added router-group-level middleware to require jwt for user and task routes
    
    3. Implement proper error response for protected routes
    -respond with 401 errors if 'no token provided' or 'invalid token'

---
+++ NodeJS112/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const taskRoutes = require('../routes/task');
+const userRoutes = require('../routes/user');
+const dbConnection = require('../utils/db');
+
+const app = express();
+
+(async () => {
+    try {
+        await dbConnection();
+        app.listen(3000, console.log('Server listening on port 3000.'));
+        app.use('/', taskRoutes);
+        app.use('/', userRoutes);
+    } catch (error) {
+        console.error(error);
+    };
+})();
